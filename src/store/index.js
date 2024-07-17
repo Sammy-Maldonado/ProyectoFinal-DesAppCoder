82e31661ac6989506e60e6ff5fff49dc1698e71c
Sammy Maldonado <sammyalejandro.m.p@gmail.com>
2024-07-16 22:53:39 -0400

    redux

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/Counter/CounterSlice";
+
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+  }
+})
